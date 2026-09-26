26131f02d9334b660a3d4999a3684209361afb70
alltoolforest <thesaleforest@gmail.com>
2026-09-26 19:49:31 +0530

    Pair deleted PDF list rows with their markers

--- documents/edit-pdf/fortress/src/export/exporter.js
+++ documents/edit-pdf/fortress/src/export/exporter.js
@@ -1,7 +1,9 @@
 import { exportEditedPdf as exportEditedPdfCore } from './exporter-core.js';
 import { applyUpwardCompaction } from './upward-compaction-v3.js';
+import { addDeletionMarkerCompanions } from './list-marker-companions.js';
 
 export async function exportEditedPdf(originalBytes,transactions,options={}){
-  const result=await exportEditedPdfCore(originalBytes,transactions,options);
-  return applyUpwardCompaction(result,transactions,{preview:!!options.preview});
+  const preparedTransactions=await addDeletionMarkerCompanions(originalBytes,transactions);
+  const result=await exportEditedPdfCore(originalBytes,preparedTransactions,options);
+  return applyUpwardCompaction(result,preparedTransactions,{preview:!!options.preview});
 }
